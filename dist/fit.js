ee4aa000f8d2e737ed8090a6b56b94de543677a9
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-04 21:05:42 +0200

    Compile src

--- dist/fit.js
+++ dist/fit.js
@@ -1300,14 +1300,8 @@ var FIT = exports.FIT = {
     },
     timer_trigger: {
       0: 'manual',
-      1: 'time',
-      2: 'distance',
-      3: 'position_start',
-      4: 'position_lap',
-      5: 'position_waypoint',
-      6: 'position_marked',
-      7: 'session_end',
-      8: 'fitness_equipment',
+      1: 'auto',
+      2: 'fitness_equipment'
     },
     fitness_equipment_state: {
       0: 'ready',
