0c2a2a85561111fae86932765da090cb4cbc3aac
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-04-21 17:37:37 +0200

    chore: add more session and lap stats

--- src/fit.js
+++ src/fit.js
@@ -305,6 +305,11 @@ export const FIT = {
       129: { field: 'avg_lev_motor_power', type: 'uint16', scale: null, offset: 0, units: 'watts' },
       130: { field: 'max_lev_motor_power', type: 'uint16', scale: null, offset: 0, units: 'watts' },
       131: { field: 'lev_battery_consumption', type: 'uint8', scale: 2, offset: 0, units: 'percent' },
+      132: { field: 'avg_vertical_ratio', type: 'uint16', scale: 100, offset: 0, units: 'percent' },
+      133: { field: 'avg_stance_time_balance', type: 'uint16', scale: 100, offset: 0, units: 'percent' },
+      134: { field: 'avg_step_length', type: 'uint16', scale: 10, offset: 0, units: 'mm' },
+      137: { field: 'total_anaerobic_effect', type: 'uint8', scale: 10, offset: 0, units: '' },
+      139: { field: 'avg_vam', type: 'uint16', scale: 1000, offset: 0, units: 'm/s' },
     },
     19: {
       name: 'lap',
@@ -410,6 +415,10 @@ export const FIT = {
       115: { field: 'avg_lev_motor_power', type: 'uint16', scale: null, offset: 0, units: 'watts' },
       116: { field: 'max_lev_motor_power', type: 'uint16', scale: null, offset: 0, units: 'watts' },
       117: { field: 'lev_battery_consumption', type: 'uint8', scale: 2, offset: 0, units: 'percent' },
+      118: { field: 'avg_vertical_ratio', type: 'uint16', scale: 100, offset: 0, units: 'percent' },
+      119: { field: 'avg_stance_time_balance', type: 'uint16', scale: 100, offset: 0, units: 'percent' },
+      120: { field: 'avg_step_length', type: 'uint16', scale: 10, offset: 0, units: 'mm' },
+      121: { field: 'avg_vam', type: 'uint16', scale: 1000, offset: 0, units: 'm/s' },
     },
     20: {
       name: 'record',
