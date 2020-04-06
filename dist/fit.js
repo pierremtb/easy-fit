847a05d6a9551cad0d01a86be3b91f84a37207b4
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-04-06 09:56:06 +0200

    chore: compile

--- dist/fit.js
+++ dist/fit.js
@@ -5,34 +5,61 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.getMessageName = getMessageName;
 exports.getFieldObject = getFieldObject;
+// some unit conversion constants
+var metersInOneKilometer = 1000;
+var secondsInOneHour = 3600;
+// according to https://en.wikipedia.org/wiki/Mile
+var metersInOneMile = 1609.344;
+
 var FIT = exports.FIT = {
   scConst: 180 / Math.pow(2, 31),
   options: {
     speedUnits: {
-      mph: {
-        multiplier: 3.6 / 1.4,
+      // native speed unit: meters per second [m/s]
+      'm/s': {
+        multiplier: 1,
         offset: 0
       },
+      // miles per hour [mph]
+      mph: {
+        multiplier: secondsInOneHour / metersInOneMile,
+        offset: 0
+      },
+      // kilometers per hour [km/h]
       'km/h': {
-        multiplier: 3.6,
+        multiplier: secondsInOneHour / metersInOneKilometer,
         offset: 0
       }
     },
     lengthUnits: {
-      mi: {
-        multiplier: 1 / 1609.344,
+      // native length unit: meters [m]
+      m: {
+        multiplier: 1,
         offset: 0
       },
+      // (international) mile [mi]
+      mi: {
+        multiplier: 1 / metersInOneMile,
+        offset: 0
+      },
+      // kilometer [km]
       km: {
-        multiplier: 1 / 1000,
+        multiplier: 1 / metersInOneKilometer,
         offset: 0
       }
     },
     temperatureUnits: {
+      // native temperature unit: degree Celsius [°C]
+      '°C': {
+        multiplier: 1,
+        offset: 0
+      },
+      // kelvin [K]
       kelvin: {
         multiplier: 1,
         offset: -273.15
       },
+      // degree fahrenheit [°F]
       fahrenheit: {
         multiplier: 9 / 5,
         offset: 32
